4aa18b3f34a05f8194d4f84125cf3955e915974c
learn-git <alaminhossen176466@gmail.com>
2026-05-02 09:17:12 +0600

    fix: handle duplicate user registration and improve error handling

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,25 +2,35 @@ import { prisma } from "../../../lib/prisma";
 import bcrypt from "bcrypt";
 
 const createUserInDb = async (payload: any) => {
-  const hasPassword = await bcrypt.hash(payload.password, 10);
+  const hashedPassword = await bcrypt.hash(payload.password, 10);
 
-  const result = await prisma.user.create({
-    data: {
-      name: payload.name,
-      email: payload.email,
-      phone: payload.phone,
-      password: hasPassword,
-    },
-    select: {
-      id: true,
-      name: true,
-      email: true,
-      phone: true,
-      role: true,
-      createdAt: true,
-    },
+  const existUser = await prisma.user.findUnique({
+    where: { email: payload.email },
   });
-  return result;
+
+  if (existUser) {
+    throw new Error("user already registered");
+  }
+
+  try {
+    const result = await prisma.user.create({
+      data: {
+        name: payload.name,
+        email: payload.email,
+        phone: payload.phone,
+        password: hashedPassword,
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        phone: true,
+        role: true,
+        createdAt: true,
+      },
+    });
+    return result;
+  } catch (error) {}
 };
 
 export const UserService = {
