15ea6aec1a327bdc4ea3b0ebd9c0c29e4ad661c5
learn-git <alaminhossen176466@gmail.com>
2026-05-17 18:42:18 +0600

    chagne app.ts file

--- src/app.ts
+++ src/app.ts
@@ -1,21 +1,29 @@
 import express from "express";
-import dotenv from "dotenv";
-import cors from "cors";
-import userRoute from "./modules/user/user.route";
-import authRouter from "./modules/auth/auth.route";
-import tutorRoute from "./modules/tutor/tutor.route";
-import bookingRoute from "./modules/booking/booking.route";
-import reviewRoute from "./modules/review/review.route";
+// import dotenv from "dotenv";
+// import cors from "cors";
+// import userRoute from "./modules/user/user.route";
+// import authRouter from "./modules/auth/auth.route";
+// import tutorRoute from "./modules/tutor/tutor.route";
+// import bookingRoute from "./modules/booking/booking.route";
+// import reviewRoute from "./modules/review/review.route";
 
-dotenv.config();
+// // dotenv.config();
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+
+// app.use("/api/v1/users", userRoute);
+// app.use("/api/v1/auth", authRouter);
+// app.use("/api/v1/tutor", tutorRoute);
+// app.use("/api/v1/bookings", bookingRoute);
+// app.use("/api/v1/reviews", reviewRoute);
+
+// export default app;
+
 const app = express();
-app.use(cors());
-app.use(express.json());
 
-app.use("/api/v1/users", userRoute);
-app.use("/api/v1/auth", authRouter);
-app.use("/api/v1/tutor", tutorRoute);
-app.use("/api/v1/bookings", bookingRoute);
-app.use("/api/v1/reviews", reviewRoute);
+app.get("/", (req, res) => {
+  res.json({ ok: true });
+});
 
 export default app;
